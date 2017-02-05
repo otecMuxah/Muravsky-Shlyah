11d7c3705bd629c0f077f39b797b81efe6d56d24
Misha <suxx_2000@hotmail.com>
2017-02-05 11:19:27 +0200

    email

--- static/assets/php/send_form.php
+++ static/assets/php/send_form.php
@@ -5,5 +5,5 @@ $adres= $_POST['adres'];
 $emailTo = 'suxx_2000@hotmail.com'; //Сюда введите Ваш email
 $body = "$fio \n\n$phone\n\n$email \n\n$adres";
 $headers = "Content-Type: text/plain; charset=utf-8\r\n".'From: MySite <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
-mail($emailTo, $fio, $body, $headers);
+mail($emailTo, $headers, $fio, $body, $headers);
 $emailSent = true;
